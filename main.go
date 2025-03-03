89d1f972563b8401ca1690ca662314670c2f41cc
Cygran <apcanning90@gmail.com>
2025-03-03 10:48:55 +0000

    add health check endpoint and serve static files under /app

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
