de4a84cb05e28f1148764c60a13736c8d773416d
Cygran <apcanning90@gmail.com>
2025-03-03 10:34:52 +0000

    initialize project with basic structure and server setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cygran/chirpy
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
