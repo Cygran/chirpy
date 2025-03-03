e9301d957f93f4f835525ed63546f28425ad8f14
Cygran <apcanning90@gmail.com>
2025-03-03 15:25:28 +0000

    add profanity filtering to validateChirpHandler and update response format

--- validate.go
+++ validate.go
@@ -3,14 +3,31 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+func cleanProfanity(input string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(input, " ")
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+		for _, profane := range profaneWords {
+			if lowerWord == profane {
+				words[i] = "****"
+				break
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
+}
 
 func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,7 +44,9 @@ func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	cleanedText := cleanProfanity(params.Body)
+
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleanedText,
 	})
 }
