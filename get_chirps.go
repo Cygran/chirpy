fdf44545ff17e97f317f651035e6bb4a2ab82694
Cygran <apcanning90@gmail.com>
2025-03-05 10:36:08 +0000

    add functionality to retrieve chirps by author ID and implement corresponding database query

--- get_chirps.go
+++ get_chirps.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/cygran/chirpy/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) getChirpsHandler(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.dbQueries.GetChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
-		return
+	var (
+		dbChirps []database.Chirp
+		err      error
+	)
+
+	idString := r.URL.Query().Get("author_id")
+	if idString != "" {
+		authorId, err := uuid.Parse(idString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author_id", err)
+			return
+		}
+		dbChirps, err = cfg.dbQueries.GetChirpsByUserId(r.Context(), authorId)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrive chirps by author", err)
+			return
+		}
+	} else {
+		dbChirps, err = cfg.dbQueries.GetChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+			return
+		}
 	}
 
 	chirps := []Chirp{}
